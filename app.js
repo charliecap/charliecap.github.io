14f845a769dd9a0930cc0dfd7460dcc2275b835b
Charlie Cline <charliecap@users.noreply.github.com>
2020-06-17 20:28:08 -0400

    .

---
+++ app.js
@@ -0,0 +1,19 @@
+const now = new Date();
+const before = new Date("May 1 2020 10:00:00");
+const update = dateFns.distanceInWords(now, before, {addSuffix: true});
+
+const html = `
+<span>Created ${update}</span>
+`;
+
+document.querySelector('#footer').innerHTML += html;
+
+window.onload = function () {
+  document.body.classList.remove("is-preload");
+};
+window.ontouchmove = function () {
+  return false;
+};
+window.onorientationchange = function () {
+  document.body.scrollTop = 0;
+};
