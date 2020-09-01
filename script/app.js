6e75061eb84afcc1bbf86294a1683a13237a834b
Charlie Cline <charlesclinesdesk@gmail.com>
2020-09-01 19:47:00 -0400

    .

---
+++ script/app.js
@@ -0,0 +1,23 @@
+// change fixed nav on small screen to a responsive hamburger menu and align with main nav on large page (use the menu from the alt page )
+// want the scroll animation added for element selection from nav
+// maybe change the company name to charliecap or a version of that? sub for the monalisa idea
+
+/* logo change
+interactive nav sub in
+carousel updated
+project links and images
+*/
+// const that = document.querySelector("body");
+
+
+// that.addEventListener('onload', function () {
+//     const videoElement = document.getElementById('vid');
+//     if (videoElement.playing) {
+//         // video is already playing so do nothing
+//     }
+//     else {
+//         // video is not playing
+//         // so play video now
+//         videoElement.play();
+//     }
+// });
